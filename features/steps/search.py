4cbd36230b5e34d23c7efe42cd9cb98fb9afe97a
Logan8192 <logan8192@protonmail.com>
2020-06-27 22:11:08 -0500

    Added PyHamCrest for more detailed assertions

--- features/steps/search.py
+++ features/steps/search.py
@@ -1,5 +1,6 @@
 from behave import given, when, then
 from POM.pages.home_page import HomePage
+from hamcrest import *
 
 
 @given('I am on the home page')
@@ -13,7 +14,8 @@ def home_page_verification(context):
 def search_website(context):
     home_page = context.current_page
     home_page.fill_search_input("Python")
-    assert home_page.return_search_input_text() == 'Python'
+    assert_that(home_page.return_search_input_text(), is_(equal_to('Python')),
+                reason="Text contained in the search input was not the expected")
     search_results_page = home_page.click_search_button()
     context.current_page = search_results_page
 
@@ -21,6 +23,9 @@ def search_website(context):
 @then('The search results page shows me a list of different websites related with the word "Python"')
 def check_website_results(context):
     search_results_page = context.current_page
-    assert search_results_page.is_page_loaded() is True
-    assert search_results_page.return_search_input_text() == "Python"
-    assert search_results_page.are_page_results_displayed() is True
+    assert_that(search_results_page.is_page_loaded(), equal_to(True),
+                reason="Search Results Page is not loaded after clicking the search button")
+    assert_that(search_results_page.return_search_input_text(), equal_to("Python"),
+                reason="Text contained in the search input was not the expected")
+    assert_that(search_results_page.are_page_results_displayed(), equal_to(True),
+                reason="There are not page results displayed after searching for 'Python'")
